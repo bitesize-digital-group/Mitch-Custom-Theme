c51717f0e6886cd283cd6b03c455c8dc96ac4993
Mitch Alforque <malforque717@gmail.com>
2019-04-07 23:34:04 +0800

    updates

--- custom.js
+++ custom.js
@@ -11,38 +11,11 @@ $(function(){
         $('.chat-button-second-content').removeClass('active');
     });
 
-    $('.tabs').tabs();
-
-    $('select').formSelect();
-
-    $('div.lazyload').lazyload();
-    $('img.lazyload').lazyload();
-
     $('.sidenav').sidenav({
         menuWidth: 300,
         closeOnClick: true,
         edge: 'right'
     });
-
-    // var autoplay = true;
-    $('.carousel.carousel-slider').carousel({
-        fullWidth: true,
-        indicators: true,
-        onCycleTo: function(element) {
-            var image = $(element).data('src');
-            $(element).css('background-image', 'url('+image+')');
-            $(element).removeAttr('data-src');
-        }
-    });
-    setInterval(function(){
-            $('.carousel.carousel-slider').carousel('next');
-    }, 4500);
-
-    // $('.carousel.carousel-slider').hover(function(){
-    //     autoplay = false;
-    // }, function(){
-    //     autoplay = true;
-    // });
 
     $('#scrollUp').click(function(){
 		$("html, body").animate({scrollTop:0},"slow");
@@ -58,11 +31,5 @@ $(function(){
         } else {
             $('#scrollUp').removeClass('d-none');
         }
-        // $('.etereman-logo').css('display', "none");
-        // clearTimeout($.data(this, "scrollCheck"));
-    
-        // $.data(this, "scrollCheck", setTimeout(function(){
-        //     $('.etereman-logo').css('display', "block");
-        // }, 250));
     });
 });
